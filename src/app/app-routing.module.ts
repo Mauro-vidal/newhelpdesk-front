6116bc9f4e920e1d7ceb755b9bba12bef90611e2
Mauro-vidal <mauroviidal@gmail.com>
2023-12-28 14:55:35 -0300

    Rota para TECNICO-LIST

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './componentes/nav/nav.component';
 import { HomeComponent } from './componentes/home/home.component';
+import { TecnicoListComponent } from './componentes/tecnico/tecnico-list/tecnico-list.component';
 
 const routes: Routes = [
   {
     path: '', component : NavComponent, children:[
-        {path: 'home', component: HomeComponent}
+        {path: 'home', component: HomeComponent},
+        {path: 'tecnicos', component: TecnicoListComponent}
     ]
   }
 ];
