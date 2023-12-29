8808dd5e5deee25ed325b16d3129d6dcf39285f3
Mauro-vidal <mauroviidal@gmail.com>
2023-12-29 01:08:08 -0300

    autentificação de login.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { NavComponent } from './componentes/nav/nav.component';
 import { HomeComponent } from './componentes/home/home.component';
 import { TecnicoListComponent } from './componentes/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './componentes/login/login.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {
-    path: '', component : NavComponent, children:[
+    path: '', component : NavComponent, canActivate: [AuthGuard], children:[
         {path: 'home', component: HomeComponent},
         {path: 'tecnicos', component: TecnicoListComponent}
     ]
---
+++ src/app/config/api.config.ts
@@ -0,0 +1,3 @@
+export const API_CONFIG = {
+    baseUrl: 'https://newhelpdesk-prod-c6fe6e65fb7b.herokuapp.com'
+}
