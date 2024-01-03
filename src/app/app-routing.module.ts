03918a7fbca6ae1fb2170c3f18aca0ea1d514946
Mauro-vidal <mauroviidal@gmail.com>
2024-01-03 11:27:34 -0300

    Rota para componente TECNICO-CREATE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,17 @@ import { HomeComponent } from './componentes/home/home.component';
 import { TecnicoListComponent } from './componentes/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
+import { TecnicoCreateComponent } from './componentes/tecnico-create/tecnico-create.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {
     path: '', component : NavComponent, canActivate: [AuthGuard], children:[
         {path: 'home', component: HomeComponent},
-        {path: 'tecnicos', component: TecnicoListComponent}
+
+        {path: 'tecnicos', component: TecnicoListComponent},
+        {path: 'tecnicos/create', component: TecnicoCreateComponent}
+         
     ]
   }
 ];
