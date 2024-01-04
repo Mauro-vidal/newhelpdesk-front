4b00429d111d89fb11893e3f64e2e143332591d6
Mauro-vidal <mauroviidal@gmail.com>
2024-01-04 01:17:48 -0300

    Componente TECNICO-UPDATE implementado.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { TecnicoListComponent } from './componentes/tecnico/tecnico-list/tecnico
 import { LoginComponent } from './componentes/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
 import { TecnicoCreateComponent } from './componentes/tecnico-create/tecnico-create.component';
+import { TecnicoUpdateComponent } from './componentes/tecnico-update/tecnico-update.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
@@ -14,7 +15,8 @@ const routes: Routes = [
         {path: 'home', component: HomeComponent},
 
         {path: 'tecnicos', component: TecnicoListComponent},
-        {path: 'tecnicos/create', component: TecnicoCreateComponent}
+        {path: 'tecnicos/create', component: TecnicoCreateComponent},
+        {path: 'tecnicos/update/:id', component: TecnicoUpdateComponent},
          
     ]
   }
