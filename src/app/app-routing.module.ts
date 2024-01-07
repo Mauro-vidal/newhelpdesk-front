e5dac48a343e4510ba676a38b2527e641d2037f2
Mauro-vidal <mauroviidal@gmail.com>
2024-01-07 01:07:29 -0300

    Rota para componente CHAMADO-CREATE.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import { ClienteCreateComponent } from './componentes/cliente/cliente-create/cli
 import { ClienteUpdateComponent } from './componentes/cliente/cliente-update/cliente-update.component';
 import { ClienteDeleteComponent } from './componentes/cliente/cliente-delete/cliente-delete.component';
 import { ChamadoListComponent } from './componentes/chamado/chamado-list/chamado-list.component';
+import { ChamadoCreateComponent } from './componentes/chamado/chamado-create/chamado-create.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
@@ -31,6 +32,7 @@ const routes: Routes = [
         {path: 'clientes/delete/:id', component: ClienteDeleteComponent},
 
         {path: 'chamados', component: ChamadoListComponent},
+        {path: 'chamados/create', component: ChamadoCreateComponent},
         
          
     ]
