365b1a3a40be06d7054ec7c34e5615e3aca2c17d
Mauro-vidal <mauroviidal@gmail.com>
2024-01-08 16:06:02 -0300

    Rota para componente CHAMADO-UPDATE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ import { ClienteUpdateComponent } from './componentes/cliente/cliente-update/cli
 import { ClienteDeleteComponent } from './componentes/cliente/cliente-delete/cliente-delete.component';
 import { ChamadoListComponent } from './componentes/chamado/chamado-list/chamado-list.component';
 import { ChamadoCreateComponent } from './componentes/chamado/chamado-create/chamado-create.component';
+import { ChamadoUpdateComponent } from './componentes/chamado/chamado-update/chamado-update.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
@@ -33,6 +34,7 @@ const routes: Routes = [
 
         {path: 'chamados', component: ChamadoListComponent},
         {path: 'chamados/create', component: ChamadoCreateComponent},
+        {path: 'chamados/update/:id', component: ChamadoUpdateComponent},
         
          
     ]
